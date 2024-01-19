2054bc734f17579ac0fb2194eb44282ce45d3556
Andrey Soroko <supersoroko@gmail.com>
2024-01-19 20:27:00 +0300

    init

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,3 @@
+export default function Customers() {
+  return <p className="text-5xl">Customers page</p>;
+}
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,3 @@
+export default function Invoices() {
+  return <p className="text-5xl">Invoices Page</p>;
+}
